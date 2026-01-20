089279c4bdab752ffe32fd6e3819cad999928799
Mariia Zueva <maryzueva@gmail.com>
2026-01-20 11:16:24 +0100

    Labels migration

---
+++ .changeset/wise-forks-care.md
@@ -0,0 +1,5 @@
+---
+"@platforma-open/milaboratories.clonotype-enrichment.ui": patch
+---
+
+Labels migration
--- ui/src/app.ts
+++ ui/src/app.ts
@@ -1,7 +1,7 @@
 import { model } from '@platforma-open/milaboratories.clonotype-enrichment.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
+import { watch } from 'vue';
 import MainPage from './pages/MainPage.vue';
-// import volcanoPage from './pages/volcanoPage.vue';
 import BubblePage from './pages/BubblePage.vue';
 import LinePage from './pages/LinePage.vue';
 import StackedPage from './pages/StackedPage.vue';
@@ -21,3 +21,12 @@ export const sdkPlugin = defineApp(model, (app) => {
 });
 
 export const useApp = sdkPlugin.useApp;
+
+// Make sure labels are initialized
+const unwatch = watch(sdkPlugin, ({ loaded }) => {
+  if (!loaded) return;
+  const app = useApp();
+  app.model.args.customBlockLabel ??= '';
+  app.model.args.defaultBlockLabel ??= 'Select Clonotype Definition';
+  unwatch();
+});
